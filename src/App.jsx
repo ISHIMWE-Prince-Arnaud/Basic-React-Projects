16b69f4f3fe7f47854d61df0ec457de85c1b2e7e
ISH k pro <princeishimwe754@gmail.com>
2025-03-03 19:03:24 +0200

    Add link to task_manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import TaskManager from "./Projects/Task Manager/TaskManager";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TaskManager from "./Projects/Task Manager/TaskManager";
+import Home from "./Projects/Home";
 
 function App() {
     return (
         <Router>
             <Routes>
+                    <Route path="/" element={<Home />} />
                 <Route path="/task_manager" element={<TaskManager />} />
             </Routes>
         </Router>
---
+++ src/Projects/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <Link to="/task_manager">Go to Task Manager</Link>
+    </>
+  );
+}
+
+export default Home;
